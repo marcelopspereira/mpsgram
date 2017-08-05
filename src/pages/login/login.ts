0f559c58dc8edcd68a688d00de2e789718b0b8af
unknown <marcelopspereira@gmail.com>
2017-08-05 11:02:58 -0300

    Cadatrando e Logando com a integração do angulafire2 e firebase OK

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { HomePage } from '../home/home';
 import { SignupPage } from "../signup/signup";
 
 /**
@@ -15,12 +18,52 @@ import { SignupPage } from "../signup/signup";
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public form: FormGroup;
+  constructor(private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private alertCtrl: AlertController) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(160),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+
+  submit() {
+    let loader = this.loadingCtrl.create({ content: 'Autenticando' });
+    loader.present();
+
+    this.afAuth.auth
+      .signInWithEmailAndPassword(
+      this.form.controls['email'].value,
+      this.form.controls['password'].value)
+      .then(() => {
+        loader.dismiss();
+        this.navCtrl.setRoot(HomePage);
+      })
+      .catch(() => {
+        loader.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Ops algo deu errado!',
+          subTitle: 'Não foi possível realizar seu cadastro',
+          buttons: ['OK']
+        });
+        alert.present();
+      });
   }
 
 
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
+
 
 /**
  * Generated class for the SignupPage page.
@@ -14,12 +18,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public form: FormGroup;
+  constructor(private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private alertCtrl: AlertController) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(160),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
+  submit() {
+    let loader = this.loadingCtrl.create({ content: 'Cadastrando' });
+    loader.present();
 
+    this.afAuth.auth
+      .createUserWithEmailAndPassword(
+      this.form.controls['email'].value,
+      this.form.controls['password'].value)
+      .then(() => {
+        loader.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Bem vindo!',
+          subTitle: 'Seu cadastro foi criado com sucesso e você já tem acesso ao nosso App.',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.setRoot(LoginPage);
+
+      })
+      .catch(() => {
+        loader.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Autentticação Inválida!',
+          subTitle: 'Usuário ou Senha incorretos',
+          buttons: ['OK']
+        });
+        alert.present();
+      });
+  }
+
+  gotToLogin() {
+    this.navCtrl.setRoot(LoginPage);
+  }
 }
