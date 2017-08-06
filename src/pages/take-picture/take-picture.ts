8540cd9222bdb261f629e3241f72b6a52bc7298a
unknown <marcelopspereira@gmail.com>
2017-08-05 21:58:37 -0300

    tirando foto concluído

--- src/pages/take-picture/take-picture.ts
+++ src/pages/take-picture/take-picture.ts
@@ -20,12 +20,31 @@ export class TakePicturePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TakePicturePage');
+
+    var video = <any>document.getElementById('video');
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+        })
+    }
   }
 
   takePicture() {
-    let modal = this.modalCtrl.create(SendPhotoPage);
-    modal.present();
+    var video = <any>document.getElementById('video');
+    var canvas = <any>document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, 320, 240);
 
+    video.classList.add('animated');
+    video.classList.add('flash');
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+
+      let modal = this.modalCtrl.create(SendPhotoPage, { photo: canvas.toDataURL() });
+      modal.present();
+    }, 800);
   }
   dismiss() {
     this.viewCtrl.dismiss();
